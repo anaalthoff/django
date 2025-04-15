6fee9f9d93ccc761053b4edf075f020889da341f
Ana Paula Althoff <ana.althoff@srssistemas.com>
2025-04-15 17:51:16 -0300

    feat: adiciona nova rota para exibir a data e atualiza .gitignore

--- primeirosite/views.py
+++ primeirosite/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+import datetime
 
 def index(request):
     return HttpResponse('Olá, mundo!')
+
+def qual_dia(request):
+    dia_formatado = datetime.datetime.now().strftime('%d/%m/%Y')
+    html = "<html><body>Hoje é dia %s.</body></html>" % dia_formatado
+    return HttpResponse(html)
